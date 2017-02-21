a2455fff251891a6ec93f7e0a364dd9c2455a7bd
Strauss72 <loicguen@gmail.com>
2017-02-21 15:00:39 +0100

    TP3 fonctionnel

--- Java/pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ Java/pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -6,6 +6,11 @@ public class PizzeriaAdminConsoleApp {
 	public static void main(String args[]){		
 		Scanner sc = new Scanner(System.in);
 		
+		int choix = 0; 
+		int indice = 0;
+
+		String[] nouvellePizza = new String[3];
+		
 		String[][] pizzas = {	{"PEP","Pépéroni",		"12.50"},
 								{"MAR","Margherita",	"14.00"},
 								{"REI","La Reine",		"11.50"},
@@ -14,63 +19,101 @@ public class PizzeriaAdminConsoleApp {
 								{"SAV","La savoyarde",	"13.00"},
 								{"ORI","L'orientale",	"13.50"},
 								{"IND","L'indienne",	"14.50"}};
+		
+		int taille = pizzas.length;
 
-		System.out.println("***** Pizzeria Administration *****");
+		System.out.println("\n***** Pizzeria Administration *****");
 		System.out.println("1. Lister les pizzas");
 		System.out.println("2. Ajouter une nouvelle pizza");
 		System.out.println("3. Mettre à jour une pizza");
 		System.out.println("4. Supprimer une pizza");
-		System.out.println("99. sortir\n");
+		System.out.println("99. sortir");
 		
-		int choix = 0; 
 		
 		while( (choix = sc.nextInt())!= 99 ){			
 			
 			switch(choix){
 				case 1:
-					AfficherPizzas(pizzas);
+					System.out.println("\n");
+					
+					for(String[] pizza : pizzas){
+						System.out.println(pizza[0]+" -> "+pizza[1]+" ("+pizza[2]+")");
+					}
+					
 					break;
-				case 2:
-					pizzas = AjouterPizza(pizzas);
+				case 2:				
+					System.out.println("Veuiller saisir le code");
+					nouvellePizza[0] = sc.nextLine(); 
+					System.out.println("Veuiller saisir le nom (sans espace)");
+					nouvellePizza[1] = sc.nextLine(); 		
+					System.out.println("Veuiller saisir le prix");
+					nouvellePizza[2] = sc.nextLine(); 
+					
+					String[][] pizzass = Arrays.copyOf(pizzas,taille+1);
+					pizzass[taille]=nouvellePizza;
+					pizzas = pizzass;
+					taille++;
 					break;
+					
+				case 3:
+					for(String[] pizza : pizzas){
+						System.out.println(pizza[0]+" -> "+pizza[1]+" ("+pizza[2]+")");
+					}					
+					
+					System.out.println("Veuiller choisir la pizza à modifier.\n(99 pour abandonner).");		
+					
+					if ((indice = sc.nextInt()) == 99){
+						break;
+					}
+					
+					System.out.println("Veuiller saisir le code");
+					nouvellePizza[0] = sc.nextLine(); 
+					System.out.println("Veuiller saisir le nom (sans espace)");
+					nouvellePizza[1] = sc.nextLine(); 		
+					System.out.println("Veuiller saisir le prix");
+					nouvellePizza[2] = sc.nextLine(); 
+					
+					pizzas[indice]=nouvellePizza;	
+					
+					break;
+					
+				case 4:
+					String[][] nouvellesPizzas = new String[taille-1][3];
+					for(String[] pizza : pizzas){
+						System.out.println(pizza[0]+" -> "+pizza[1]+" ("+pizza[2]+")");
+					}					
+					
+					System.out.println("Veuiller choisir la pizza à supprimer.\n(99 pour abandonner).");		
+					
+					if ((indice = sc.nextInt()) == 99){
+						break;
+					} 
+					
+					
+					System.arraycopy(pizzas, 0, nouvellesPizzas, 0, indice);
+					System.arraycopy(pizzas, indice+1, nouvellesPizzas, indice, taille-1-indice);
+					pizzas = nouvellesPizzas;
+					taille--;
+					break;					
 			}
 			
-			System.out.println("***** Pizzeria Administration *****");
+			System.out.println("\n***** Pizzeria Administration *****");
 			System.out.println("1. Lister les pizzas");
 			System.out.println("2. Ajouter une nouvelle pizza");
 			System.out.println("3. Mettre à jour une pizza");
 			System.out.println("4. Supprimer une pizza");
-			System.out.println("99. sortir\n");
+			System.out.println("99. sortir");
 			
 		}
-	}
-	
-	private static void AfficherPizzas(String[][] pizzas){
-		for(String[] pizza : pizzas){
-			System.out.println(pizza[0]+" -> "+pizza[1]+" ("+pizza[2]+")1");
-		}
-	}
-	
-	private static String[][] AjouterPizza(String[][] pizzas){
-		Scanner sc = new Scanner(System.in);
-		int taille = pizzas.length;
-		String[] pizza = new String[3];
-		
-		System.out.println("Veuiller saisir le code");
-		pizza[0] = sc.nextLine(); 
-		System.out.println("Veuiller saisir le nom (sans espace)");
-		pizza[1] = sc.nextLine(); 		
-		System.out.println("Veuiller saisir le prix");
-		pizza[2] = sc.nextLine(); 
-		
-		String[][] pizzass = Arrays.copyOf(pizzas,taille+1);
-		pizzass[taille]=pizza;
-		
 		sc.close();
-		
-		return pizzass;
 	}
 	
-	
+	private static String[][] MajPizza(Scanner sc, String[][] pizzas){
+		sc.nextLine();
+			
+		
+		return pizzas;
+		
+	}
 	
 }

2c81b095fb14c5722ac45bdcfc9b4f343ca59438
Strauss72 <loicguen@gmail.com>
2017-02-21 11:36:40 +0100

    TP1 Bonjour

---
+++ Java/TP1/Bonjour.java
@@ -0,0 +1,10 @@
+/**
+ * Classe qui dit bonjour à tout le monde
+ *
+ */
+public class Bonjour{
+    public static void main(String args[]){
+        System.out.println("Bonjour tout le monde");
+    }
+}
+

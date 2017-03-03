a1d68febd74141b270451bd2d33da8c24d456a6c
Strauss72 <loicguen@gmail.com>
2017-03-03 14:49:23 +0100

    maven migration

---
+++ Java/Pizzeria/src/test/java/fr/pizzeria/dao/PizzaDaoImplTest.java
@@ -0,0 +1,25 @@
+package fr.pizzeria.dao;
+
+public class PizzaDaoImplTest {
+	private PizzaDaoImpl pizzaDao;
+
+	/*
+	 * @Before public void setUp() { pizzaDao = new PizzaDaoImpl(); }
+	 * 
+	 * @Test public void testFindAllPizzas() { fail("j'echoue"); }
+	 * 
+	 * @Test public void test_find_all() { List<Pizza> pizzas =
+	 * pizzaDao.findAllPizzas(); assertEquals(9, pizzas.size()); }
+	 * 
+	 * @Test public void test_save() throws StockageException { Pizza pizza =
+	 * new Pizza(null, null, 0, null); pizzaDao.saveNewPizza(pizza); List<Pizza>
+	 * pizzas = pizzaDao.findAllPizzas(); assertEquals(8, pizzas.size());
+	 * assertThat(pizzas.size(), is(8)); }
+	 * 
+	 * @Test public void test_save_valid() throws StockageException { Pizza
+	 * pizza = new Pizza("TEST", "Test", 1.2, CategoriePizza.POISSON);
+	 * pizzaDao.saveNewPizza(pizza); List<Pizza> pizzas =
+	 * pizzaDao.findAllPizzas(); assertEquals(9, pizzas.size()); }
+	 */
+
+}

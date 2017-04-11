03e726972a93fe4962712ab3cfcd0ba675222b72
Strauss72 <loicguen@gmail.com>
2017-04-11 16:43:08 +0200

    gitignore

---
+++ JavaScript/pizzajs/pizzang/app/alert/index.js
@@ -0,0 +1,23 @@
+import angular from 'angular';
+
+export const AlertModule = angular.module('alert.module', [])
+
+.service('AlertService', class AlertService {
+    constructor($timeout) {
+        this.alert = {
+            message: ''
+        }
+        this.$timeout = $timeout;
+    }
+    addAlert(message) {
+        this.alert.message = message;
+        this.$timeout(3000).then(() => this.alert.message = '');
+    }
+})
+.controller('AlertController', class AlertController {
+    constructor(AlertService) {
+        this.alert = AlertService.alert;
+    }
+})
+
+.name;
